085b8e6f65e7ce98bddb80b2b238824841602390
PUO <86181292+kl204@users.noreply.github.com>
2024-03-22 09:07:45 +0900

    first commit

---
+++ src/main/java/com/example/seleniumcrwalingtest/SeleniumCrwalingTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.seleniumcrwalingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumCrwalingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeleniumCrwalingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/seleniumcrwalingtest/SeleniumCrwalingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seleniumcrwalingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumCrwalingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
